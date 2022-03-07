1a399f7b9193260575f7dc89168c285aff767cc5
KushagraGitter <kushagra305@gmail.com>
2022-03-07 17:53:21 +0530

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,60 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkList {
+  constructor(node) {
+    this.head = node;
+    this.ll_size = 1;
+  }
+
+  inset_start(node) {
+    node.next = this.head;
+    this.head.prev = node;
+    this.head = node;
+  }
+
+  inset_end(node) {
+    let temp = this.head;
+    while (temp.next !== null) {
+      temp = temp.next;
+    }
+    temp.next = node;
+    node.prev = temp;
+    temp = node;
+  }
+
+  delete_start() {
+    this.head = this.head.next;
+    this.head.prev = null;
+  }
+
+  delete_end() {
+    let last = this.head;
+    while (last.next !== null) {
+      last = last.next;
+    }
+    last.prev.next = null;
+    last.prev = null;
+  }
+
+  first() {
+    return this.head;
+  }
+
+  last() {
+    let last = this.head;
+    while (last.next !== null) {
+      last = last.next;
+    }
+    return last;
+  }
+}
